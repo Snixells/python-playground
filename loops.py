1c025e9f96eccb93ec76d1bae2b6e216ea6b83cf
Sebastian Schäfer <bschr9uwb@googlemail.com>
2020-03-22 11:00:58 +0100

    Numpy Array loop test

---
+++ loops.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79]) 
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7]) 
+meas = np.array([np_height, np_weight]) 
+
+for val in np.nditer(meas):
+    print(val)
+
+
+for index, val in np.ndenumerate(meas):
+    print(index)
+    print(val)
